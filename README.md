e39402ac55d7db0b9106b153998566d5b4dfc8e4
Dmitry K <dimka.rs@gmail.com>
2022-04-01 23:03:03 +0300

    Alien art is live!

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Useful info: https://github.com/Xinyuan-LilyGO/TTGO-T-Display
 
+Some code taken from this project: https://github.com/nopnop2002/esp-idf-st7789
+
+Alien art: https://hackaday.com/2021/04/13/alien-art-drawn-with-surprisingly-simple-math/
+
+Board bought here: https://www.aliexpress.com/item/33048962331.html
+
 ![pinout](images/ttgo.jpg)
--- main/main.c
+++ main/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "st7789.h"
 
+#define TAG "main"
+#define DELAY 200
+
+static void AlienTest(TFT_t * dev, int width, int height, int key)
+{
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            int16_t color = WHITE;
+            if ((x ^ y) % key) color = BLACK;
+            lcdDrawPixel(dev, x, y, color);
+        }
+    }
+    //vTaskDelay(50); //sort of yield
+}
+
+static void ST7789(void *pvParameters)
+{
+    TFT_t lcd;
+
+    spi_master_init(&lcd, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO, CONFIG_BL_GPIO);
+    lcdInit(&lcd, CONFIG_WIDTH, CONFIG_HEIGHT, CONFIG_OFFSETX, CONFIG_OFFSETY);
+
+    while(1)
+    {
+        AlienTest(&lcd, CONFIG_WIDTH, CONFIG_HEIGHT, 9);
+        AlienTest(&lcd, CONFIG_WIDTH, CONFIG_HEIGHT, 17);
+        AlienTest(&lcd, CONFIG_WIDTH, CONFIG_HEIGHT, 33);
+    }
+}
 
 void app_main(void)
 {
-    printf("Hello!\r\n");
+    ESP_LOGI(TAG,"Hello!");
+
+    xTaskCreate(ST7789, "ST7789", 1024*6, NULL, 0, NULL);
 }
